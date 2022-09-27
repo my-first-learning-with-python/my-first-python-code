27090df14a670cc8e1375d60417f756066867ca1
dzikr <dzikrikovalesky12@gmail.com>
2022-09-27 14:36:33 +0700

    membuat singkat variabel

--- dasar_sintaksis_perulangan_dengan_while_undefine_count.py
+++ dasar_sintaksis_perulangan_dengan_while_undefine_count.py
@@ -4,23 +4,23 @@ program perulangan dengan while sampai paham (Undefined count)
 jumlah_buku = 10
 print(f"Jumlah buku yang inin dibaca adalah {jumlah_buku} buku")
 
-jumlah_buku_dibaca_dan_dipahami = 0
-print(f"Jumlah buku yang sudah dibaca sebanyak {jumlah_buku_dibaca_dan_dipahami} buku\n")
+jumlah_baca = 0
+print(f"Jumlah buku yang sudah dibaca sebanyak {jumlah_baca} buku\n")
 
-total_jumlah_baca = 0
+jumlah_paham = 0
 
-while total_jumlah_baca < jumlah_buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if jumlah_buku_dibaca_dan_dipahami == 9:
-        print(f"buku ke {jumlah_buku_dibaca_dan_dipahami + 1} Belum paham")
+while jumlah_paham < jumlah_buku * 2:
+    jumlah_paham = jumlah_paham + 1
+    if jumlah_baca == 9:
+        print(f"buku ke {jumlah_baca + 1} Belum paham")
     else:
-        jumlah_buku_dibaca_dan_dipahami = jumlah_buku_dibaca_dan_dipahami +1
-        print(f"jumlah buku ke {jumlah_buku_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+        jumlah_baca = jumlah_baca + 1
+        print(f"jumlah buku ke {jumlah_baca} sudah dibaca dan dipahami")
 
-print("jumlah buku yang sudah dibaca", jumlah_buku_dibaca_dan_dipahami )
+print("jumlah buku yang sudah dibaca", jumlah_baca)
 
-if jumlah_buku_dibaca_dan_dipahami == jumlah_buku:
+if jumlah_baca == jumlah_buku:
     print("semua buku budi telah baca dan pahami")
 else:
-    print(f"maaf bu tidak semua buku bisa dipahami, budi hanya bisa memahami {jumlah_buku_dibaca_dan_dipahami} buku")
+    print(f"maaf bu tidak semua buku bisa dipahami, budi hanya bisa memahami {jumlah_baca} buku")
 
