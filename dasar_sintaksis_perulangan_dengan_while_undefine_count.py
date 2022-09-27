31b3e890726d272bc6ab12b8f0e179d21bcba095
dzikr <dzikrikovalesky12@gmail.com>
2022-09-27 14:37:51 +0700

    rename in english

--- dasar_sintaksis_perulangan_dengan_while_undefine_count.py
+++ dasar_sintaksis_perulangan_dengan_while_undefine_count.py
@@ -1,26 +1,26 @@
 """
 program perulangan dengan while sampai paham (Undefined count)
 """
-jumlah_buku = 10
-print(f"Jumlah buku yang inin dibaca adalah {jumlah_buku} buku")
+book_count = 10
+print(f"Jumlah buku yang inin dibaca adalah {book_count} buku")
 
-jumlah_baca = 0
-print(f"Jumlah buku yang sudah dibaca sebanyak {jumlah_baca} buku\n")
+read_count = 0
+print(f"Jumlah buku yang sudah dibaca sebanyak {read_count} buku\n")
 
-jumlah_paham = 0
+understood_count = 0
 
-while jumlah_paham < jumlah_buku * 2:
-    jumlah_paham = jumlah_paham + 1
-    if jumlah_baca == 9:
-        print(f"buku ke {jumlah_baca + 1} Belum paham")
+while understood_count < book_count * 2:
+    understood_count = understood_count + 1
+    if read_count == 9:
+        print(f"buku ke {read_count + 1} Belum paham")
     else:
-        jumlah_baca = jumlah_baca + 1
-        print(f"jumlah buku ke {jumlah_baca} sudah dibaca dan dipahami")
+        read_count = read_count + 1
+        print(f"jumlah buku ke {read_count} sudah dibaca dan dipahami")
 
-print("jumlah buku yang sudah dibaca", jumlah_baca)
+print("jumlah buku yang sudah dibaca", read_count)
 
-if jumlah_baca == jumlah_buku:
+if read_count == book_count:
     print("semua buku budi telah baca dan pahami")
 else:
-    print(f"maaf bu tidak semua buku bisa dipahami, budi hanya bisa memahami {jumlah_baca} buku")
+    print(f"maaf bu tidak semua buku bisa dipahami, budi hanya bisa memahami {read_count} buku")
 
