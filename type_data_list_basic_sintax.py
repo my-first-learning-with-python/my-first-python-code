1c2f003902dbcd1a35c0b11d7106bcd2f4b50fbe
dzikr <dzikrikovalesky12@gmail.com>
2022-09-28 10:50:18 +0700

    Memahami tipe data list dasar

---
+++ type_data_list_basic_sintax.py
@@ -0,0 +1,27 @@
+list_book = ["Atomic Habbits", "Seven Habbits", "Mindset"]
+print('tampilkan variable data buku')
+print(list_book)
+
+print('\nproses semua dengan for')
+for book in list_book:
+    print(book)
+
+print('\nproses semua dengan for')
+print(list_book[0])
+print(list_book[2])
+
+print("\ntampilkan dgn for in range")
+for i in range(0, len(list_book)):
+    print(list_book[i])
+
+print("\nTampilkan dgn for in rane, dimana datanya berbeda2")
+list_book = [1, 'mariposa', -3.23, 3.14]
+for i in range(0, len(list_book)):
+    print(list_book[i])
+
+print("\nkembalikan nilai awal list_book")
+list_book = ["Atomic Habbits", "Seven Habbits", "Mindset"]
+print("\ntambahkan 1 buku baru")
+list_book.append("fifty sade of the grey")
+for i in range(0, len(list_book)):
+    print(list_book[i])
